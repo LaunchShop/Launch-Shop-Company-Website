64c9f053ba66cb3769a6acae8bba744cb46c642a
Nick Basile <nicholas.basile@outlook.com>
2016-07-19 22:25:02 -0500

    Updated Website

--- js/main.js
+++
@@ -1,8 +0,0 @@
-jQuery(document).ready(function($){
-    $(".arrow-background").click(function() {
-    $('html, body').animate({
-        scrollTop: $("#services").offset().top
-    }, 2000);
-});
-});
-
